661dc7205d562aa381f8884cd66aa2413208afcd
Carl Kamholtz <carl.kamholtz@gmail.com>
2022-07-31 14:22:30 +1000

    Move USB logging into its own module

---
+++ light_bulb_dongle/src/usb_logging.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef USB_LOGGING_H
+#define USB_LOGGING_H 1
+
+void usb_logging_init();
+
+#endif /* USB_LOGGING_H */
+
